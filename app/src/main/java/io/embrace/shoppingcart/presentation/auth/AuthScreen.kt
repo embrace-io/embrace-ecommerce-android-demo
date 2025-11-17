10cd03a148274181c01c92e29a9dffecfd6c4fe5
nelsitoPuglisi <nelson.puglisi@protonmail.com>
2025-11-17 16:34:34 -0300

    fix log upload

--- app/src/main/java/io/embrace/shoppingcart/presentation/auth/AuthScreen.kt
+++ app/src/main/java/io/embrace/shoppingcart/presentation/auth/AuthScreen.kt
@@ -1,20 +1,32 @@
 package io.embrace.shoppingcart.presentation.auth
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import io.embrace.android.embracesdk.Embrace
-import io.embrace.shoppingcart.presentation.home.blockWithThreadSleep
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
